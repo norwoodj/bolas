ed1d2a208283ab3cd206a70fd9b089f4b4cec758
John Norwood <norwood.john.m@gmail.com>
2023-10-15 19:30:07 +0100

    chore: factors our http server binding/running code from definition of routes

---
+++ bin/bolas/src/http.rs
@@ -0,0 +1,84 @@
+use actix_http::{Request, Response};
+use actix_service::ServiceFactory;
+use actix_web::{body::MessageBody, dev::AppConfig, HttpServer};
+use libsystemd::{activation, activation::IsType};
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::io;
+use std::net::{SocketAddr, TcpListener};
+use std::os::fd::{FromRawFd, IntoRawFd};
+use std::os::unix::net::UnixListener;
+
+fn get_systemd_listeners(
+    systemd_names: &[String],
+) -> io::Result<(HashMap<String, TcpListener>, HashMap<String, UnixListener>)> {
+    let file_descriptors_with_names = activation::receive_descriptors_with_names(true)
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+    let inet_listeners = file_descriptors_with_names
+        .iter()
+        .filter(|(fd, name)| systemd_names.contains(name) && fd.is_inet())
+        .map(|(fd, name)| unsafe {
+            (
+                name.clone(),
+                TcpListener::from_raw_fd(fd.clone().into_raw_fd()),
+            )
+        })
+        .collect();
+
+    let unix_listeners = file_descriptors_with_names
+        .into_iter()
+        .filter(|(fd, name)| systemd_names.contains(name) && fd.is_unix())
+        .map(|(fd, name)| unsafe { (name, UnixListener::from_raw_fd(fd.into_raw_fd())) })
+        .collect();
+
+    Ok((inet_listeners, unix_listeners))
+}
+
+pub(crate) async fn run_http_server<F, I, S, B>(
+    mut server: HttpServer<F, I, S, B>,
+    tcp_addrs: &[SocketAddr],
+    unix_addrs: &[String],
+    systemd_names: &[String],
+) -> io::Result<()>
+where
+    F: Fn() -> I + Send + Clone + 'static,
+    I: actix_service::IntoServiceFactory<S, Request>,
+    S: ServiceFactory<Request, Config = AppConfig, Error = actix_web::Error> + 'static,
+    <S as ServiceFactory<Request>>::Error: 'static,
+    S::InitError: Debug,
+    S::Response: Into<Response<B>>,
+    B: MessageBody + 'static,
+{
+    for t in tcp_addrs {
+        log::info!("Starting HTTP server on tcp listener {}", t);
+        server = server.bind(t)?;
+    }
+
+    for u in unix_addrs {
+        log::info!("Starting HTTP server on unix listener {}", u);
+        server = server.bind_uds(u)?;
+    }
+
+    if !systemd_names.is_empty() {
+        let (tcp_listeners, unix_listeners) = get_systemd_listeners(systemd_names)?;
+
+        for (name, t) in tcp_listeners {
+            log::info!(
+                "Starting HTTP server on inherited systemd tcp listener {}",
+                name
+            );
+            server = server.listen(t)?;
+        }
+
+        for (name, u) in unix_listeners {
+            log::info!(
+                "Starting HTTP server on inherited systemd unix listener {}",
+                name
+            );
+            server = server.listen_uds(u)?;
+        }
+    }
+
+    server.run().await
+}
--- bin/bolas/src/main.rs
+++ bin/bolas/src/main.rs
@@ -1,61 +1,25 @@
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use clap::Parser;
-use libsystemd::{activation, activation::IsType};
-use std::collections::HashMap;
 use std::convert::TryInto;
 use std::io;
-use std::net::TcpListener;
-use std::os::fd::{FromRawFd, IntoRawFd};
-use std::os::unix::net::UnixListener;
 
 mod bolas;
 mod config;
+mod http;
 mod static_files;
 mod version;
 mod websocket;
 
 use self::config::{BolasArgs, BolasConfig};
+use self::http::run_http_server;
+use self::version::VersionInfo;
 
-fn get_systemd_listeners(
-    systemd_names: Vec<String>,
-) -> io::Result<(HashMap<String, TcpListener>, HashMap<String, UnixListener>)> {
-    let file_descriptors_with_names = activation::receive_descriptors_with_names(true)
-        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-
-    let inet_listeners = file_descriptors_with_names
-        .iter()
-        .filter(|(fd, name)| systemd_names.contains(name) && fd.is_inet())
-        .map(|(fd, name)| unsafe {
-            (
-                name.clone(),
-                TcpListener::from_raw_fd(fd.clone().into_raw_fd()),
-            )
-        })
-        .collect();
-
-    let unix_listeners = file_descriptors_with_names
-        .into_iter()
-        .filter(|(fd, name)| systemd_names.contains(name) && fd.is_unix())
-        .map(|(fd, name)| unsafe { (name, UnixListener::from_raw_fd(fd.into_raw_fd())) })
-        .collect();
-
-    Ok((inet_listeners, unix_listeners))
-}
-
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let args = BolasArgs::parse();
-    let version_info = version::VersionInfo::default();
-    let bolas_config: BolasConfig = match (&args).try_into() {
-        Ok(c) => c,
-        Err(e) => {
-            log::error!("Failed to convert arguments to runtime configuration {e:?}");
-            return Err(e);
-        }
-    };
-
-    let mut server = HttpServer::new(move || {
+async fn run_application_server(
+    bolas_args: BolasArgs,
+    bolas_config: BolasConfig,
+    version_info: VersionInfo,
+) -> io::Result<()> {
+    let app_server = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(bolas_config.clone()))
             .app_data(web::Data::new(version_info.clone()))
@@ -72,32 +36,27 @@ async fn main() -> io::Result<()> {
             )
     });
 
-    for t in args.tcp_addrs {
-        log::info!("Starting HTTP server on {}", t);
-        server = server.bind(t)?;
-    }
+    run_http_server(
+        app_server,
+        &bolas_args.tcp_addrs,
+        &bolas_args.unix_addrs,
+        &bolas_args.systemd_names,
+    )
+    .await
+}
 
-    for u in args.unix_addrs {
-        log::info!("Starting HTTP server on {}", u);
-        server = server.bind_uds(u)?;
-    }
-
-    if !args.systemd_names.is_empty() {
-        let (tcp_listeners, unix_listeners) = get_systemd_listeners(args.systemd_names)?;
-
-        for (name, t) in tcp_listeners {
-            log::info!(
-                "Starting HTTP server on inherited systemd tcp listener {}",
-                name
-            );
-            server = server.listen(t)?;
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let bolas_args = BolasArgs::parse();
+    let version_info = VersionInfo::default();
+    let bolas_config: BolasConfig = match (&bolas_args).try_into() {
+        Ok(c) => c,
+        Err(e) => {
+            log::error!("Failed to convert arguments to runtime configuration {e:?}");
+            return Err(e);
         }
+    };
 
-        for (name, u) in unix_listeners {
-            log::info!("Starting HTTP server on {}", name);
-            server = server.listen_uds(u)?;
-        }
-    }
-
-    server.run().await
+    run_application_server(bolas_args, bolas_config, version_info).await
 }
